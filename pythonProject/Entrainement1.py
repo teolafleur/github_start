82a6bddfcd018cbe43a121d41e1e03d85ea40ec4
teo lafleur <teolafleur@yahoo.fr>
2021-10-06 11:30:57 +0200

    premier envoi sur internet

---
+++ pythonProject/Entrainement1.py
@@ -0,0 +1,19 @@
+x = 5
+print(x)
+
+list_voiture = ['lambo','fefe','ford','camaro']
+print(list_voiture[2])
+
+del list_voiture[3]
+print(list_voiture)
+
+list_voiture.append('Maserati')
+print(list_voiture)
+
+def devine_mon_age(annee_de_naissance):
+    return 2019-annee_de_naissance;
+
+age=devine_mon_age(1993)
+print(age)
+
+list_valeur=[1,2,3]
---
+++ pythonProject/test.py
@@ -0,0 +1,73 @@
+Nom_voiture = ["Lambo","Fefe","Testla"]
+print(Nom_voiture[1])
+
+Chien = "Berger Allemand"
+print('Le chien est un ' + Chien)
+
+x=3
+s=str(x)
+e=int(s)
+print(type(s))
+print(s)
+print(type(e))
+print(e)
+
+x = "salut "
+y = "teo"
+z=x+y
+print(z)
+
+list=[1,2,3,"okey manny","manolo","okeokey"]
+list.remove(list[3])
+del list[2]
+print("Le nombre d'element de la liste est de : ", len(list))
+
+print('index de manolo : ', list.index('manolo'))
+element_list = list[-1]
+print(element_list)
+
+x=list
+print(list)
+print(x)
+
+x.append("oeoe")
+print(list)
+
+list2 = ['Chocolat','cacao','ricorai']
+list2.append('cacao2')
+
+print(9 in list)
+
+listeglobal= [list,list2]
+print(listeglobal)
+
+list.extend(list2)
+print(list)
+
+mon_tuple = (1,'ok',True)
+print(type(mon_tuple))
+
+x,y,z = 1,2,3
+
+mon_dictionnaire = {}
+mon_dictionnaire["prenom"]='Teo'
+mon_dictionnaire["nom"]='Lafleur'
+
+print(mon_dictionnaire.get("Name","cle int"))
+
+#un dictionnaire en python est une sorti
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
